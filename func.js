d76e81314a66f5ffe0b214cd8dd04ec73365bbde
Shotaro Ooki <shoutaro0726@gmail.com>
2020-05-25 00:26:45 +0900

    Commit

--- func.js
+++ func.js
@@ -35,4 +35,86 @@ function alertTweetable(text) {
     }
 };
 
+function isTweetable1(text) {
+    return text.length <= 140;
+}
+
+const isTweetable = (text) => {
+    return text.length <= 140;
+}
+
+// function bring(food) {
+//     if () {
+        
+//     }
+// }
+
+// function peel(food) {
+//     if () {
+
+//     }
+// }
+
+// function cut(food) {
+//     if () {
+
+//     }
+// }
+
+function washed(fn) {
+    if (washed === 'end') {
+        fn();
+    }
+}
+
+function bring(food) {
+
+}
+
+function peer(food) {
+
+}
+
+function cut(food) {
+
+}
+
+washed(bring);
+
+function unFollow() {
+    console.log('フォローを外しました。');
+}
+
+function cancelTweet() {
+    console.log('ツイートをキャンセルしました。');
+}
+
+function confirmed(fn) {
+    const input = prompt("実行しますか？");
+    fn(input);  
+}
+
+confirmed((input) => {
+    if (input === "実行") {
+        console.log("リポジトリを削除");
+    }
+});
+
+confirmed(unFollow);
+
+const cancelTweet = () => {
+    console.log("実行しますか?");
+}
+
+prompt("実行しますか?");
+
+const btn = document.getElementById("button");
+btn.addEventListener("click", () => {
+    console.log("フォロー解除");
+});
+
+const foods = ["にんじん", "ぽてと", "玉ねぎ"];
+foods.forEach((food) => {
+    console.log(food);
+});
 
